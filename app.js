4d04a056a2405a18f0baff284bdff2e4d7f81192
Andy <andrea_sue2@icloud.com>
2022-08-22 16:59:14 -0500

    install mongoose; scaffold app.js

--- app.js
+++ app.js
@@ -2,4 +2,19 @@ const express = require('express');
 
 const app = express();
 
+// MIDDLEWARES - function that executes when routes are hit
+// app.use('/posts', () => {
+//     console.log('hit the posts route');
+// });
+
 // ROUTES
+app.get('/', (req, res) => {
+    res.send('Home route');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('Posts route');
+});
+
+// Listen to server
+app.listen(3000);
