bca68fc9d24a3ab36b2521809c11e5e74cc8b423
Andy <andrea_sue2@icloud.com>
2022-08-23 09:43:41 -0500

    add route to get by id; add route to delete post by id

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Differences with tutorial:
 - MLab redirects to MongoDb's website but the setup is much the same
 - if you want to specify which database on your cluster to write to, you can modify your connection string following [this comment on StackOverflow](https://stackoverflow.com/questions/59098595/set-database-and-collection-using-mongoose#comment104434786_59099513); it seems like there must be a better way to do this, since the same app may want to connect to multiple databases - seems odd you'd need different connection strings for all of them, rather than just setting it as a configuration somewhere
 - if you want to specify which collection in that database to save to, [here is the Mongoose documentation](https://mongoosejs.com/docs/guide.html#collection)
+- to delete, you'll get a deprecation warning to use deleteOne instead of remove
--- routes/posts.js
+++ routes/posts.js
@@ -2,24 +2,56 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('posts route');
+router.get('/', async(req, res) => {
+    try {
+        const posts = await Post.find();
+        res.send(posts).status(200);
+    } catch (err) {
+        res.send({ message: err });
+    }
 });
 
-router.post('/', (req, res) => {
+router.get('/:id', async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.send(post).status(200);
+    } catch (err) {
+        res.status(404).send({ message: err });
+    }
+});
+
+router.post('/', async(req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description,
         image: req.body.image,
     });
-    post
-        .save()
-        .then((data) => {
-            res.send(req.body).status(200);
-        })
-        .catch((error) => {
-            res.send({ message: error });
-        });
+    try {
+        const savedPost = await post.save();
+        res.send(savedPost).status(201);
+    } catch (err) {
+        // todo: format error message better
+        res.send({ message: err });
+    }
+
+    // old school style of handling promises
+    // post
+    //     .save()
+    //     .then((data) => {
+    //         res.send(req.body).status(201);
+    //     })
+    //     .catch((error) => {
+    //         res.send({ message: error });
+    //     });
+});
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const removedPost = await Post.deleteOne({ _id: req.params.id });
+        res.send(removedPost).status(200);
+    } catch (err) {
+        res.status(404).send({ message: err });
+    }
 });
 
 module.exports = router;
